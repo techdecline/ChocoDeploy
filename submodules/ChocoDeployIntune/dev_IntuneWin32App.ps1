63a28908f09b7af91075e71626474fe5ced96b65
Cornelius Schuchardt <cschuchardt@bright-skies.de>
2020-02-21 12:26:15 +0100

    added example

--- submodules/ChocoDeployIntune/dev_IntuneWin32App.ps1
+++ submodules/ChocoDeployIntune/dev_IntuneWin32App.ps1
@@ -26,3 +26,31 @@ $Win32App = Add-IntuneWin32App -TenantName "name.onmicrosoft.com" -FilePath $Int
 
 # Add assignment for all users
 Add-IntuneWin32AppAssignment -TenantName "name.onmicrosoft.com" -DisplayName $Win32App.displayName -Target "AllUsers" -Intent "available" -Notification "showAll" -Verbose
+
+
+
+Get-Module intunewin32app
+Get-Module intunewin32app -ls
+Get-Module intunewin32app -ListAvailable
+Update-Module intunewin32app
+$cred = Get-Credential admin@schuchardtsofted.onmicrosoft.com
+Connect-AzureAD -Credential $cred
+cd .\ChocoDeploy\
+ls
+. .\function-ConvertFrom-Svg.ps1
+ConvertFrom-Svg -SourceFile C:\Code\Workbench\GoogleChrome\chrome.svg
+Import-Module .\ChocoDeploy.psd
+Import-Module .\ChocoDeploy.psd1
+ConvertFrom-Svg -SourceFile C:\Code\Workbench\GoogleChrome\chrome.svg
+$imgFile = New-IntuneWin32AppIcon -FilePath C:\Code\Workbench\GoogleChrome\chrome.jpeg
+$detectionRule = New-IntuneWin32AppDetectionRule -PowerShellScript C:\Code\Workbench\GoogleChrome\GoogleChrome_detect.ps1
+$detectionRule = New-IntuneWin32AppDetectionRule -PowerShellScript -ScriptFile "C:\Code\Workbench\GoogleChrome\GoogleChrome_detect.ps1"
+$displayName = "Google Chrome"
+Add-IntuneWin32App -TenantName "declinelab.com" -FilePath C:\Code\Workbench\GoogleChrome\GoogleChrome_install.intunewin -DisplayName $displayName -InstallCommandLine (Get-Content C:\Code\Workbench\GoogleChrome\GoogleChrome_install.cmd) -UninstallCommandLine (Get-Content C:\Code\Workbench\GoogleChrome\GoogleChrome_uninstall.cmd) -InstallExperience system -Icon $imgFile
+Add-IntuneWin32App -TenantName "declinelab.com" -FilePath C:\Code\Workbench\GoogleChrome\GoogleChrome_install.intunewin -DisplayName $displayName -InstallCommandLine (Get-Content C:\Code\Workbench\GoogleChrome\GoogleChrome_install.cmd) -UninstallCommandLine (Get-Content C:\Code\Workbench\GoogleChrome\GoogleChrome_uninstall.cmd) -InstallExperience system -Icon $imgFile -DetectionRule $detectionRule -Description "google Chrome" -Publisher Google
+Install-Module psauth -Scope CurrentUser
+Add-IntuneWin32App -TenantName "declinelab.com" -FilePath C:\Code\Workbench\GoogleChrome\GoogleChrome_install.intunewin -DisplayName $displayName -InstallCommandLine (Get-Content C:\Code\Workbench\GoogleChrome\GoogleChrome_install.cmd) -UninstallCommandLine (Get-Content C:\Code\Workbench\GoogleChrome\GoogleChrome_uninstall.cmd) -InstallExperience system -Icon $imgFile -DetectionRule $detectionRule -Description "google Chrome" -Publisher Google
+Add-IntuneWin32App -TenantName "declinelab.com" -FilePath C:\Code\Workbench\GoogleChrome\GoogleChrome_install.intunewin -DisplayName $displayName -InstallCommandLine (Get-Content C:\Code\Workbench\GoogleChrome\GoogleChrome_install.cmd) -UninstallCommandLine (Get-Content C:\Code\Workbench\GoogleChrome\GoogleChrome_uninstall.cmd) -InstallExperience system -Icon $imgFile -DetectionRule $detectionRule -Description "google Chrome" -Publisher Google -RestartBehavior suppress
+Install-Module psintuneauth -Scope CurrentUser
+Update-Module psintuneauth
+Add-IntuneWin32App -TenantName "declinelab.com" -FilePath C:\Code\Workbench\GoogleChrome\GoogleChrome_install.intunewin -DisplayName $displayName -InstallCommandLine (Get-Content C:\Code\Workbench\GoogleChrome\GoogleChrome_install.cmd) -UninstallCommandLine (Get-Content C:\Code\Workbench\GoogleChrome\GoogleChrome_uninstall.cmd) -InstallExperience system -Icon $imgFile -DetectionRule $detectionRule -Description "google Chrome" -Publisher Google -RestartBehavior suppress
